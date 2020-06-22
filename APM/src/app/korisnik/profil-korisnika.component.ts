c47deddff8a048ba3380509cec4b5ca71966baa4
milanovicn <mnina012@yahoo.com>
2020-06-22 13:15:57 +0200

    prebacen projekat na mysql bazu

--- APM/src/app/korisnik/profil-korisnika.component.ts
+++ APM/src/app/korisnik/profil-korisnika.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginServces } from '../login/login.services';
+import { Korisnik } from '../login/Korisnik';
 
 @Component({
   selector: 'pm-profil-korisnika',
@@ -9,11 +10,26 @@ import { LoginServces } from '../login/login.services';
 })
 export class ProfilKorisnikaComponent implements OnInit {
 
-  constructor(private _router: Router,private loginService:LoginServces) { }
-
+  constructor(private _router: Router,private loginService:LoginServces) { 
+    this.korisnik = new Korisnik();
+  }
+  korisnik :Korisnik;
   request:Request;
   
   ngOnInit(): void {
+    this.loginService.getKorisnika().subscribe({
+      next: korisnik => {
+        this.korisnik = korisnik;
+        console.log('Korisnik', this.korisnik);
+        if (this.korisnik == null) {
+          this._router.navigate(["/welcome"]);
+        }
+        if (this.korisnik != null && this.korisnik.rola != "CLIENT") {
+          this.loginService.IzlogujSe(this.request).subscribe(result => this.kraj());
+        }
+      }
+    });
+
   }
 
   kraj() {
