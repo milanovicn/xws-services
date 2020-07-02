18c019915eb7ebbc7859d62ac84bdd85aba93b6d
milanovicn <mnina012@yahoo.com>
2020-07-02 17:32:47 +0200

    Izmenjene greske, i html pretrage na frontu

--- services/adnin-service/src/main/java/com/example/adninservice/repository/MessageRepository.java
+++ services/adnin-service/src/main/java/com/example/adninservice/repository/MessageRepository.java
@@ -2,8 +2,14 @@ package com.example.adninservice.repository;
 
 import com.example.adninservice.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message,Long> {
+
+
 }
